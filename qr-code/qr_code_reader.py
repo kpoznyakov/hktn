cde8d13a7513c79ea9777abb8e21b6cda29597d0
Leo Emar-Kar <L.EmarKar@users.noreply.github.com>
2019-03-31 00:17:29 +0300

    Update qr_code_reader.py
    - формирование основы запроса к бд,
    на предмет наличия этого кода

--- qr-code/qr_code_reader.py
+++ qr-code/qr_code_reader.py
@@ -1,10 +1,16 @@
 # coding = "utf-8"
 """ TODO
-сервис для декодировки ID в qr-code и его проверка в базе
-данных на подлинность
+сервис для декодировки ID в qr-code и его проверка 
+в базе данных на подлинность
 """
 
+import psycopg2
 import qrdecode
 
-qr = qrdecode.decode('image.png')
-print(qr)
+qr = qrdecode.decode('guestcode.png')
+conn = psycopg2.connect(user='postgres', password='qweasdzxc',
+                        database='app', host='100.100.148.215')
+cursor = conn.cursor()
+cursor.execute("SELECT app.dbo.check_QR('{}')".format(qr))
+key = cursor.fetchone()
+print(key)
