c6498793cb48e4a92016f63742696bc4553ffd69
k.poznyakov <pozkir@gmail.com>
2019-03-30 20:48:00 +0300

    5

--- barrier/check_is_auto_comes.py
+++ barrier/check_is_auto_comes.py
@@ -2,6 +2,7 @@ import random
 import requests
 import os
 from time import sleep
+import datetime
 
 
 def check_is_car():
@@ -18,7 +19,11 @@ BASE_URL = 'https://platerecognizer.com/v1/plate-reader'
 
 def plate_recog(dir_, file):
     # print(file)
-
+    allowed_plates = [
+        's001ss01',
+        'r001am77',
+        'm5550e61'
+    ]
     with open(dir_ + file, 'rb') as fp:
         response = requests.post(
             BASE_URL,
@@ -28,8 +33,17 @@ def plate_recog(dir_, file):
     try:
         if response.json()['results'] is None:
             print(response.text)
+            pass
         else:
-            print(response.json()['results'][0]['plate'])
+            res = response.json()['results'][0]['plate']
+            # if (SELECT `status` FROM some WHERE plate = res) is 'Allow':
+            #
+            #
+            print(res)
+            if res in allowed_plates:
+                print('Проезжайте!')
+            else:
+                print('Номер не найден. Обратитесь на пункт охраны или воспользуйтесь картой доступа.')
     except IndexError:
         print('Не распознан. Воспользуйтесь пропуском.')
         pass
@@ -47,8 +61,9 @@ def select_random_file(dir_):
 
 
 if __name__ == '__main__':
-    # print(select_random_file('./plates_img/'))
+    dir_with_plates = './plates_img/'
+    # print(select_random_file(dir_with_plates))
     while True:
         if check_is_car():
-            l = select_random_file('./plates_img/')
-            plate_recog('./plates_img/', l)
+            l = select_random_file(dir_with_plates)
+            plate_recog(dir_with_plates, l)
